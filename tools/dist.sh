59486ff9e0fa57754f43d57704f12b86680e07ef
asad <aarafat.me@gmail.com>
2023-01-24 15:36:21 +0000

    dev

--- tools/dist.sh
+++ tools/dist.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Cleanup dist folder..."
 rm -rR dist/*
+mkdir dist/html-public
+touch dist/html-public/put-html-asset-here.txt
 
 echo "Build Linux Binary..."
 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o topoviewer cloudshellwrapper/cmd/main.go 
@@ -14,7 +16,6 @@ cp -r tools/clab-client-mac dist/clab-client-mac
 cp -r tools/clab-client-windows dist/clab-client-windows
 
 echo "Copy html folder and assets..."
-mkdir dist/html-public
 cp -r html-static dist/html-static
 cp rawTopoFile/topo-topoViewerDemo.yaml dist/topo-topoViewerDemo.yaml
 
