c3fb915c2b63bdc1e0ea6b615c13cf13c2db1251
Your Name <you@example.com>
2023-07-01 00:22:47 +0200

    dev-fix

--- tools/dist.sh
+++ tools/dist.sh
@@ -30,7 +30,7 @@ echo "Copy html folder and assets..."
 cp -r html-static dist/html-static
 cp rawTopoFile/topo-topoViewerDemo.yaml dist/topo-topoViewerDemo.yaml
 
-echo "Create TAR package..."
-tar -czvf dist/TopoViewer.tar.gz dist
+# echo "Create TAR package..."
+# tar -czvf dist/TopoViewer.tar.gz dist
 
 echo "Distribution build done..."
