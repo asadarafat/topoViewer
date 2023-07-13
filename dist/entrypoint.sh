164e82aec54caa2adfe336c7db9395209db09f57
Your Name <you@example.com>
2023-07-13 14:33:12 +0200

    asad

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -3,4 +3,4 @@
 # Trigger an error if non-zero exit code is encountered
 set -e 
 
-/opt/topoviewer/topoviewer clab -H ${1} -p ${2} -u ${3} -j local-bind/topo-file.yaml
+/opt/topoviewer/topoviewer clab -H 149.204.21.68  -p 8080 -u suuser -j local-bind/topo-file.yaml
