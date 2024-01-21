8d58c71525c05d2403f78f56fbe77b0ed2f585d5
Your Name <you@example.com>
2024-01-21 21:21:44 +0000

    commit with tag nightly-24.01.21p

--- go_cloudshellwrapper/constants.go
+++ go_cloudshellwrapper/constants.go
@@ -1,7 +1,7 @@
 package cloudshellwrapper
 
 <<<<<<< HEAD
-var VersionInfo string = "nightly-24.01.21o"
+var VersionInfo string = "nightly-24.01.21p"
 =======
-var VersionInfo string = "nightly-24.01.21j"
+var VersionInfo string = "nightly-24.01.21p"
 >>>>>>> 086a68742ab98e626a7a3b18058f719644aaaddd
