4a7bae9b01a8288110834d735aab34f56428a2d6
Arafat Asad <asad.arafat@nokia.com>
2024-10-21 12:56:21 +0000

    commit with tag nightly-24.10.21a

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -3,4 +3,4 @@
 # Trigger an error if non-zero exit code is encountered
 set -e 
 
-/opt/topoviewer/topoviewer clab -H 149.204.21.68  -p 8080 -u suuser -j local-bind/topo-file.yaml
+/opt/topoviewer/topoviewer clab --allowed-hostnames $ALLOWED_HOSTNAME --clab-user $CLAB_USER --clab-pass $CLAB_PASS --server-port $SERVER_PORT --topology-file-json local-bind/topo-file.json &
--- go_cloudshellwrapper/constants.go.bak
+++ go_cloudshellwrapper/constants.go.bak
@@ -1,6 +1,6 @@
 package cloudshellwrapper
 
-var VersionInfo string = "nightly-24.10.20g"
+var VersionInfo string = "nightly-24.10.21"
 
 // create html-public files
 var HtmlPublicPrefixPath string = "./html-public/"
