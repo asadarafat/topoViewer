dd77a5cfe55a58e7a4ba270c21bbe0394eda088c
Your Name <you@example.com>
2024-10-27 10:25:43 +0000

    commit with tag nightly-24.10.27

--- dist/entrypoint.sh
+++ dist/entrypoint.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-
-# Trigger an error if non-zero exit code is encountered
-set -e 
-
-# Function to check if both 'nodes' and 'links' have entries
-check_file_content() {
-  jq -e '.nodes | length > 0' local-bind/topo-file.json >/dev/null 2>&1
-}
-
-# Wait until the JSON file contains the required 'nodes' and 'links' entries
-while ! check_file_content; do
-  echo "Waiting for 'nodes' and 'links' entries in local-bind/topo-file.json..."
-  sleep 5  # Wait for 5 seconds before checking again
-done
-
-# Once the content is found, execute the command
-/opt/topoviewer/topoviewer clab --allowed-hostnames $ALLOWED_HOSTNAME --clab-user $CLAB_USER --clab-pass $CLAB_PASS --server-port $SERVER_PORT --topology-file-json local-bind/topo-file.json &
+/opt/topoviewer/topoviewer clab --allowed-hostnames $ALLOWED_HOSTNAME --clab-user $CLAB_USER --clab-pass $CLAB_PASS --server-port $SERVER_PORT --topology-file-yaml local-bind/$CLAB_TOPO_YAML &
--- go_cloudshellwrapper/constants.go.bak
+++ go_cloudshellwrapper/constants.go.bak
@@ -1,6 +1,6 @@
 package cloudshellwrapper
 
-var VersionInfo string = "nightly-24.10.26b"
+var VersionInfo string = "nightly-24.10.26c"
 
 // create html-public files
 var HtmlPublicPrefixPath string = "./html-public/"
