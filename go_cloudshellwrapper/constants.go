7d7b6864ef1ed77356c1701f47f84f91649cc215
Your Name <you@example.com>
2024-12-24 15:53:21 +0000

    commit with tag nightly-24.12.24

--- go_cloudshellwrapper/constants.go
+++ go_cloudshellwrapper/constants.go
@@ -1,6 +1,6 @@
 package cloudshellwrapper
 
-var VersionInfo string = "nightly-24.12-24"
+var VersionInfo string = "nightly-24.12.24"
 
 // create html-public files
 var HtmlPublicPrefixPath string = "./html-public/"
