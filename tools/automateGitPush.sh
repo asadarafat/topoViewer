0ed4d0f6e31e93be2eb170b9e634250180ffeba5
Your Name <you@example.com>
2024-12-29 15:07:34 +0000

    Automated commit on 2024-12-29 15:07:28

--- tools/automateGitPush.sh
+++ tools/automateGitPush.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
+# Parse optional commit message
+COMMIT_MESSAGE="Automated commit on $(date +'%Y-%m-%d %H:%M:%S')"
+while getopts "m:" opt; do
+  case $opt in
+    m)
+      COMMIT_MESSAGE=$OPTARG
+      ;;
+    *)
+      echo "Usage: $0 [-m <commit-message>]"
+      exit 1
+      ;;
+  esac
+done
+
 # Change ownership of all files in the current directory recursively to the current user
 sudo chown -R $(whoami):$(whoami) *
 
 # Commit changes to the Git repository
 git add .
-git commit -m "Automated commit on $(date +'%Y-%m-%d %H:%M:%S')"
+git commit -m "$COMMIT_MESSAGE"
 
 # Push changes to the remote repository
 git push
