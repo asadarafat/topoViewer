803e87d00d01bee94e91976b09a63d8a3da7b29d
Asad Arafat <asad.arafat@nokia.com>
2026-06-28 21:16:10 +0200

    test(vscode): register local monaco suggestions

--- packages/vscode-topoviewer/src/webview/monacoSetup.ts
+++ packages/vscode-topoviewer/src/webview/monacoSetup.ts
@@ -2,6 +2,7 @@ import { loader } from '@monaco-editor/react';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
 import 'monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution';
+import 'monaco-editor/esm/vs/editor/contrib/suggest/browser/suggestController';
 
 globalThis.MonacoEnvironment = {
   getWorker() {
